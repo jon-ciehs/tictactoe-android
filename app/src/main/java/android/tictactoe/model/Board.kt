c2285c4f51c737df99d6d81ccc379e680cc8361b
Jon <blancorodriguez.jonathan@gmail.com>
2022-02-18 17:57:55 +0000

    Model completed

--- app/src/main/java/android/tictactoe/model/Board.kt
+++ app/src/main/java/android/tictactoe/model/Board.kt
@@ -20,31 +20,71 @@ class Cell(var player: Player? = null) {
 class Board() {
 
     private val sqSize: Int = 3
-    val board: Array<Array<Cell>> = Array(sqSize) { Array(sqSize) { Cell() } }
-    var state: GameState? = null
-    var turn: Player? = Player.X
+
+    lateinit var board: Array<Array<Cell>>
+    lateinit var state: GameState
+    lateinit var turn: Player
+
+    init {
+        restart()
+    }
+
+    fun restart() {
+        board = Array(sqSize) { Array(sqSize) { Cell() } }
+        state = GameState.ALIVE
+        turn = Player.X
+    }
+
+    fun getCell(row: Int, column: Int) : Cell {
+        if (!validGet(row) || !validGet(column)) { throw IllegalArgumentException("Invalid position in the grid") }
+        return board[row][column]
+    }
 
     fun move(row: Int, column: Int): Boolean {
         return validMove(row, column).also {
             if (it) {
-                getCell(row, column)?.player = turn
-                changeTurn()
+                getCell(row, column).player = turn
+                if (checkWinner()) { state = GameState.ENDED } else changeTurn()
             }
         }
     }
 
-    private fun changeTurn() { turn = if (turn == Player.X) Player.O else Player.X }
+    private fun checkWinner(): Boolean {
+        for (row in 0 until sqSize) {
+            for (column in 0 until sqSize) {
+                val player: String = getCellValue(row, column)
 
-    private fun getCell(row: Int, column: Int) : Cell? {
-        if (!validGet(row) || !validGet(column)) { throw IllegalArgumentException("Invalid position in the grid") }
-        return board[row][column]
+                if (player == "-") continue
+
+                if (column + 2 < sqSize &&
+                        player == getCellValue(row, (column+1)) &&
+                        player == getCellValue(row, (column+2))) { return true }
+
+                if (row + 2 < sqSize) {
+                    if (player == getCellValue(row+1, column) &&
+                        player == getCellValue(row+2, column)) { return true }
+
+                    if (column + 2 < sqSize &&
+                        player == getCellValue(row+1, (column+1)) &&
+                        player == getCellValue(row+2, (column+2))) { return true }
+
+                    if (column - 2 >= 0 &&
+                        player == getCellValue(row+1, (column-1)) &&
+                        player == getCellValue(row+2, (column-2))) { return true }
+                }
+            }
+        }
+        return false
     }
+
+    private fun getCellValue(row: Int, column: Int): String {
+        return getCell(row, column).toString()
+    }
+
+    private fun changeTurn() { turn = if (turn == Player.X) Player.O else Player.X }
 
     private fun validGet(value: Int): Boolean = value in 0 until sqSize
 
     private fun validMove(row: Int, column: Int): Boolean = getCell(row, column).toString() == "-"
 
 }
-
-
-
